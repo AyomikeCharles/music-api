bb8137cd31de5c3ca23bb587c7686bc32c9b91bf
AyomikeCharles <charlesayomike4@gmail.com>
2023-06-04 20:11:15 +0100

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
-require('dotenv').config()
 const cors = require('cors')
+require('dotenv').config()
 
 const port = process.env.PORT || 3300
 const app = express()
-
 app.use(cors({
     origin:'*'
 }))
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.listen(port, ()=>{
-    console.log(`runing on port ${port}`)
+    console.log(`listening at port ${port}`)
 })
